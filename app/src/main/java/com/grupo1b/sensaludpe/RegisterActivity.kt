acbe0764bd080e9288df10fda407526f39f8c8d3
Lesandrod <lentrix12345@gmail.com>
2022-10-18 11:52:48 -0500

    cambio colores

--- app/src/main/java/com/grupo1b/sensaludpe/RegisterActivity.kt
+++ app/src/main/java/com/grupo1b/sensaludpe/RegisterActivity.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
         val logdir=findViewById<TextView>(R.id.logindir)
         logdir.setOnClickListener{
             val lanzar = Intent(this,MainActivity::class.java)
