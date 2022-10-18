6caf5d18f98065a1882c88a96b6501f9db3fee63
Lesandrod <lentrix12345@gmail.com>
2022-10-18 11:35:10 -0500

    cambio colores

--- app/src/main/java/com/grupo1b/sensaludpe/MainActivity.kt
+++ app/src/main/java/com/grupo1b/sensaludpe/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.grupo1b.sensaludpe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val regdir=findViewById<TextView>(R.id.registrodir)
+
+        regdir.setOnClickListener{
+
+            val lanzar = Intent(this,RegisterActivity::class.java)
+            startActivity(lanzar)
+        }
+
     }
 }
--- app/src/main/java/com/grupo1b/sensaludpe/RegisterActivity.kt
+++ app/src/main/java/com/grupo1b/sensaludpe/RegisterActivity.kt
@@ -1,11 +1,18 @@
 package com.grupo1b.sensaludpe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val logdir=findViewById<TextView>(R.id.logindir)
+        logdir.setOnClickListener{
+            val lanzar = Intent(this,MainActivity::class.java)
+            startActivity(lanzar)
+        }
     }
 }
